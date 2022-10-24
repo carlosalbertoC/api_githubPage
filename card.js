366a3e69cd559a9cbafa1e6d57a81e100f9ec972
carlosalbertoC <carlosalberto6211@gmail.com>
2022-10-23 19:15:46 -0500

    checkpoint 2

--- card.js
+++ card.js
@@ -1,15 +1,10 @@
-import { datosExp } from "./index.js";
-console.log(datosExp);
-
-
-
-class Card extends HTMLElement {
-    constructor() {
+export class Card extends HTMLElement {
+    constructor(_photo,_name,_description,_linkGitHub) {
         super();
-        this.photo = "";
-        this.name = "";
-        this.description = "";
-        this.linkGitHub = "";
+        this.photo = _photo;
+        this.name = _name;
+        this.description = _description;
+        this.linkGitHub = _linkGitHub;
     }
 
     static get observedAttributes() {
@@ -22,18 +17,22 @@ class Card extends HTMLElement {
 
     connectedCallback() {
         // console.log(datosExp);
-        // this.render();
+        this.render();
 
     }
 
     render(){ return `
-    <div class="card" style="width: 18rem;">
-        <img src="..." class="card-img-top" alt="...">
-        <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+    <div class="row row-cols-1 row-cols-md-2 g-4">
+        <div class="col">
+            <div class="card" style="width: 18rem;">
+                <img src="${this.photo}" class="card-img-top" alt="...">
+            <div class="card-body">
+            <h5 class="card-title">${this.name}</h5>
+            <p class="card-text">${this.description}</p>
+            <a href="${this.linkGitHub}" class="btn btn-primary">Go somewhere</a>
         </div>
+    </div>
+    </div>
     </div>`
 }
 
--- index.js
+++ index.js
@@ -1,33 +1,51 @@
+import { Card } from "./card.js";
+
 const buscador = document.querySelector("#buscador");
 const button = document.querySelector("#button_buscador")
+const myCard = document.querySelector("my-card")
 
 const url = "https://api.github.com/users/";
 
 let nombre;
 let user;
-let datosExp = "fsdf";
+let datosExp;
 
-async function buscarProgramador(user) {
-    const data = await fetch(user);
+
+async function buscarProgramador(usX) {
+    const data = await fetch(usX);
                             console.log(data);
     const datos = await data.json();
                             // console.log("DATOS",datos);
     datosExp = datos;
-            console.log("DATOSdentroAsync",datosExp);
-    return datosExp
+
+                            console.log("DATOSdentroAsync",datosExp);
+    return datosExp;
+    
     
 }
+
 
 // datosExp = buscarProgramador(user);
 //                             console.log(datosExp);
 
-button.addEventListener("click", function () {
+button.addEventListener("click", async function () {
     nombre = buscador.value;
     user = url + nombre;
                             console.log("USER",user);
 
-    buscarProgramador(user);
-                            console.log("DATOS:",user);
+    let dataProfile = await buscarProgramador(user);
+                            console.log("DATOS:",dataProfile);
+                            console.log("DATOS:",dataProfile.avatar_url);
+    
+
+        //return ["photo","name","description","linkGitHub"];
+    let profile = new Card(dataProfile.avatar_url, dataProfile.login, dataProfile.bio, dataProfile.html_url);
+                            console.log(profile.linkGitHub);
+
+
+    myCard.innerHTML += profile.render();
+    // profile.render();
+
     
 })
 
