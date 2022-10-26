f231350c923dac1139636993a2ad09a55b5e0428
carlosalbertoC <carlosalberto6211@gmail.com>
2022-10-26 17:59:15 -0500

    listo

--- index.js
+++ index.js
@@ -13,34 +13,24 @@ let datosExp;
 
 async function buscarProgramador(usX) {
     const data = await fetch(usX);
-                            console.log(data);
     const datos = await data.json();
-                            // console.log("DATOS",datos);
     datosExp = datos;
 
-                            console.log("DATOSdentroAsync",datosExp);
     return datosExp;
     
     
 }
 
 
-// datosExp = buscarProgramador(user);
-//                             console.log(datosExp);
-
 button.addEventListener("click", async function () {
     nombre = buscador.value;
     user = url + nombre;
-                            console.log("USER",user);
 
     let dataProfile = await buscarProgramador(user);
-                            console.log("DATOS:",dataProfile);
-                            // console.log("DATOS:",dataProfile.avatar_url);
     
 
         //return ["photo","name","description","linkGitHub"];
     let profile = new Card(dataProfile.avatar_url, dataProfile.login, dataProfile.bio, dataProfile.html_url);
-                            console.log(profile.photo);
 
 
     myCard.innerHTML += `${profile.render()}`;
